adf855bf43f664bf226066834ef7d4445940c38c
Leonardo Azopardo <lazopard@gmail.com>
2014-12-08 01:06:23 -0500

    fixed bug that did not update storyboard

---
+++ Equally Wed/BudgetViewController.h
@@ -0,0 +1,13 @@
+//
+//  BudgetViewController.h
+//  Equally Wed
+//
+//  Created by Leonardo Azopardo on 12/8/14.
+//  Copyright (c) 2014 Leonardo Azopardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BudgetViewController : UIViewController
+
+@end
